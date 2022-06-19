b8d6778e7ff3952984822afeb650c6f093e2f9c4
MADO <mydodo1120@naver.com>
2022-06-19 11:22:06 +0900

    html모듈, 엔트리구동 모듈 통합
    html-builder.js, builder.js를 통합시켰습니다.

--- local_modules/builder.js
+++ local_modules/builder.js
@@ -50,5 +50,19 @@ module.exports = {
         app.quit()
             }
         })
-    }
+    },
+    Makehtml : function(url, files) {
+        console.log("* ENTRY BUILDER HTML - Ready Module.")
+      const fs = require("fs")
+          const content = `
+      <body style="overflow:hidden;">
+      <script>window.location.href = "${url}"</script> 
+      </body>
+      `
+      fs.writeFile(files, content, err => {
+              if(err) return console.log("등록 도중 오류가 발생했습니다.");
+            })
+            console.log("* ENTRY BUILDER HTML - SUSSESSFUL!")
+      }
 }
+  
