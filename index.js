75dfc203602524a5ab5271dfe509968128fc1942
MADO <62548380+dodo1120@users.noreply.github.com>
2022-08-17 20:33:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ Builder.Makehtml("https://playentry.org/iframe/614604f17fe742d742af0bf4", file)
 //엔트리 URL을 file(./index.html)에 알맞게 작성하여 html의 형식에 맞게 코드를 구성합니다.
 //순서대로 엔트리 iframe링크, 파일위치 입니다.
 
-Builder.information("제목", "./local_modules/entry.png",file)
+Builder.information("제목", `${_dirname}/local_modules/entry.png`,file)
 //프로그램의 제목("제목")을 지정하고, 아이콘의 사진을 정해 file(./index.html)과 electron을 구성시킵니다.
 //순서대로 제목, 아이콘위치, 파일위치 입니다.
