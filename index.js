d5db940fc2a15fbc3d56b933a206c45c9029b8ef
MADO <mydodo1120@naver.com>
2022-09-10 00:50:10 +0900

    2.0.0 업데이트
    함수와 여러가지 기능들의 안정화 등등 많은 개편이 이루어졌습니다.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Builder = require("./local_modules/builder") //자체 모듈 엔트리 구동용 electron 코드 불러오기
 
-const file = "./index.html"; //엔트리 구동용 html의 파일 위치를 지정합니다.
-Builder.Makehtml("https://playentry.org/iframe/614604f17fe742d742af0bf4", file)
-//엔트리 URL을 file(./index.html)에 알맞게 작성하여 html의 형식에 맞게 코드를 구성합니다.
-//순서대로 엔트리 iframe링크, 파일위치 입니다.
-
-Builder.information("제목", `${__dirname}/local_modules/entry.png`,file)
-//프로그램의 제목("제목")을 지정하고, 아이콘의 사진을 정해 file(./index.html)과 electron을 구성시킵니다.
-//순서대로 제목, 아이콘위치, 파일위치 입니다.
+Builder.siteinfo({url : "https://playentry.org/iframe/614604f17fe742d742af0bf4"})
+//Builder.siteinfo({url : "Link", files : `${__dirname}/temp.html`})
+//siteinfo의 주석에 사용법이 입력되어 있습니다.
+//url(iframe링크)는 필수로 입력하셔양 합니다.
+Builder.info({title : "에엑따"})
+//Builder.info({title : "제목", iconFile : `${__dirname}/icon.png`, size : 1.1, files : `${__dirname}/temp.html})
+//info의 주석에 사용법이 입력되어 있습니다.
+//title(제목)은 필수로 앱력하셔야 합니다.
--- local_modules/builder.js
+++ local_modules/builder.js
@@ -1,11 +1,26 @@
+const fs = require('fs');
 module.exports = {
-    information : function(title, dirnameicon, files) {
-        const fs = require('fs')
+    /**
+     * 엔트리 빌더의 정보를 설정합니다.
+      @param {String} iconFile 프로그램의 제목을 입력하는 란입니다.
+      @param {String} title 프로그램에 아이콘을 적용할 위치를 설정하는 란입니다. (필수)
+      @param {String} files 연결용 html을 불러올 경로를 지정하는 란입니다.
+      @param {String} size 프로그램 크기를 설정합니다. 기본 크기는 1이며, 소수를 사용할 수 있습니다.  (주의! 사이즈가 1.3초과시 창이 이상하게 나타날 수 있습니다. )
+      @todo 이 항목들(title 제외)은 입력하지 않을 경우 기본데이터로 대체됩니다.
+     */
+    info : function({title, iconFile, files, size}) {
+        if(!title) {
+            console.error("ERROR! : title is not found.")
+            return process.exit()
+        }
+        if(isNaN(size)) var size = 1
+        if(!iconFile) var iconFile = `${__dirname}/entry.png`
+        if(!files) var files = `${__dirname}/temp.html`
+    
 
     //코드 변경시 문제가 발생할 수 있습니다.
     //기본 설정된 값이 불편하신분만 변경해주세요
     const {app, BrowserWindow} = require('electron')
-    const path = require('path')
     const url = require('url')
 
     let win
@@ -23,7 +38,7 @@ module.exports = {
      console.log("* ELECTRON is ON!")
     }
     function createWindow() {
-    win = new BrowserWindow({width: 1105, height: 700, resizable: false, autoHideMenuBar:true, title : `${title}`, icon: `${dirnameicon}`, fullscreenable: false})
+    win = new BrowserWindow({width: 1105*size, height: 690*size, resizable: false, autoHideMenuBar:true, title : `${title}`, icon: `${iconFile}`, fullscreenable: false})
  
     win.loadURL(url.format({
         pathname: files,
@@ -50,18 +65,49 @@ win.on('page-title-updated', (evt) => {
             }
         })
     },
-    Makehtml : function(url, files) {
+        /**
+     * URL을 연결할 파일을 생성하는 함수입니다.
+      @param {String} url 엔트리 작품의 iframe링크를 입력하는 란입니다.
+      @param {String} files 연결용 html을 생성할 경로를 지정하는 란입니다.
+      @todo 이 항목들(title 제외)은 입력하지 않을 경우 기본데이터로 대체됩니다.
+     */
+     siteinfo : function({url, files}) {
+        if(!files) var files = `${__dirname}/temp.html`
+
+        if(!url) {
+            console.error("ERROR! : URL is not found.")
+            return process.exit()
+        }
         console.log("* ENTRY BUILDER HTML - Ready Module.")
-      const fs = require("fs")
           const content = `
       <body style="overflow:hidden;">
       <script>window.location.href = "${url}"</script> 
       </body>
       `
       fs.writeFile(files, content, err => {
-              if(err) return console.log("등록 도중 오류가 발생했습니다.");
+              if(err) return console.log("ERROR! : Could not create html.\n임시파일을 만들 수 없었습니다.");
             })
             console.log("* ENTRY BUILDER HTML - SUSSESSFUL!")
+      },
+    /**
+     * 엔트리 빌더의 정보를 설정합니다. (구형)
+     * @deprecated 이 함수는 entry-builder v2.0에서는 지원되지 않습니다. 대신 info를 사용하실 수 있습니다. 자세한 내용은 info의 설명을 참고하세요.
+     */
+      infomation : function(title, dirnameicon, files) {
+        return console.error("이 코드는 사용되지 않습니다. 대신 info를 사용하실 수 있습니다.\nThis code is deprecated. You can use 'info' instead.")
+      },
+    /**
+     * 엔트리 빌더의 정보를 설정합니다. (구형)
+     * @deprecated 이 함수는 entry-builder v2.0에서는 지원되지 않습니다. 대신 siteinfo를 사용하실 수 있습니다. 자세한 내용은 siteinfo의 설명을 참고하세요.
+     */
+      Makehtml : function(url, files) {
+        return console.error("이 코드는 사용되지 않습니다. 대신 siteinfo를 사용하실 수 있습니다.\nThis code is deprecated. You can use 'siteinfo' instead.")
       }
+}
+function mkdir( dirPath ) {
+    const isExists = fs.existsSync( dirPath );
+    if( !isExists ) {
+        fs.mkdirSync( dirPath, { recursive: true } );
+    }
 }
   
