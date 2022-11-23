cdb05b94bbde57ebcc90e859f5223a5a8c802706
MADO <62548380+dodo1120@users.noreply.github.com>
2022-11-23 14:34:00 +0900

    Update 3.0.0

--- local_modules/builder.js
+++ local_modules/builder.js
@@ -5,22 +5,23 @@ module.exports = {
       @param {String} iconFile 프로그램의 제목을 입력하는 란입니다.
       @param {String} title 프로그램에 아이콘을 적용할 위치를 설정하는 란입니다. (필수)
       @param {String} files 연결용 html을 불러올 경로를 지정하는 란입니다.
-      @param {String} size 프로그램 크기를 설정합니다. 기본 크기는 1이며, 소수를 사용할 수 있습니다.  (주의! 사이즈가 1.3초과시 창이 이상하게 나타날 수 있습니다. )
+      @param {String} size 프로그램 크기를 설정합니다. 기본 크기는 10이며, 소수를 사용할 수 있습니다.  (주의! 사이즈가 20초과시 창이 이상하게 나타날 수 있습니다. )
       @todo 이 항목들(title 제외)은 입력하지 않을 경우 기본데이터로 대체됩니다.
      */
     info : function({title, iconFile, files, size}) {
         if(!title) {
-            console.error("ERROR! : title is not found.")
-            return process.exit()
+            new Error("title is not found.");
         }
-        if(isNaN(size)) var size = 1
+        if(isNaN(size)) var size = 10
         if(!iconFile) var iconFile = `${__dirname}/entry.png`
         if(!files) var files = `${__dirname}/temp.html`
+        var size = size*0.1
     
 
     //코드 변경시 문제가 발생할 수 있습니다.
     //기본 설정된 값이 불편하신분만 변경해주세요
-    const {app, BrowserWindow} = require('electron')
+    const dns = require('dns')
+    const {app, BrowserWindow, dialog } = require('electron')
     const url = require('url')
 
     let win
@@ -33,6 +34,10 @@ module.exports = {
         protocol: 'file:',
         slashes: true
     }))
+
+    dns.resolve('playentry.org', function(err, addr){
+      if(err) return dialog.showMessageBox(null,{type:'warning',title:'오류', message:'엔트리에 연결할 수 없어요.'}) })
+
     console.log("* ENTRY BUILDER ELECTRON - REGISTERING! (3/3)")
     win.on('closed', () => {
         win = null
@@ -63,25 +68,14 @@ win.on('page-title-updated', (evt) => {
         if(!files) var files = `${__dirname}/temp.html`
 
         if(!url) {
-            console.error("ERROR! : URL is not found.")
+            new Error("URL is not found.");
             return process.exit()
         }
         console.log("* ENTRY BUILDER HTML - Ready Module.")
       const content = `
       <body style="overflow:hidden;">
       <script>
-      const alertOnlineStatus = () => {
-        if(!navigator.onLine) {
-        window.alert('엔트리에 접속할 수 없습니다! 인터넷 연결을 확인하고 프로그램을 다시 실행해 주세요!')
-        } else {
-            window.location.href = "${url}"
-        }
-      }
-    
-      window.addEventListener('online',  alertOnlineStatus)
-      window.addEventListener('offline',  alertOnlineStatus)
-
-      alertOnlineStatus()
+      window.location.href = "${url}"
       </script> 
       </body>
       `
@@ -92,16 +86,16 @@ win.on('page-title-updated', (evt) => {
       },
     /**
      * 엔트리 빌더의 정보를 설정합니다. (구형)
-     * @deprecated 이 함수는 entry-builder v2.0에서는 지원되지 않습니다. 대신 info를 사용하실 수 있습니다. 자세한 내용은 info의 설명을 참고하세요.
+     * @deprecated 이 함수는 entry-builder v2.0부터는 지원되지 않습니다. 대신 info를 사용하실 수 있습니다. 자세한 내용은 info의 설명을 참고하세요.
      */
       infomation : function(title, dirnameicon, files) {
-        return console.error("이 코드는 사용되지 않습니다. 대신 info를 사용하실 수 있습니다.\nThis code is deprecated. You can use 'info' instead.")
+        new Error("이 코드는 사용되지 않습니다. 대신 info를 사용하실 수 있습니다.\nThis code is deprecated. You can use 'info' instead.");
       },
     /**
      * 엔트리 빌더의 정보를 설정합니다. (구형)
-     * @deprecated 이 함수는 entry-builder v2.0에서는 지원되지 않습니다. 대신 siteinfo를 사용하실 수 있습니다. 자세한 내용은 siteinfo의 설명을 참고하세요.
+     * @deprecated 이 함수는 entry-builder v2.0부터는 지원되지 않습니다. 대신 siteinfo를 사용하실 수 있습니다. 자세한 내용은 siteinfo의 설명을 참고하세요.
      */
       Makehtml : function(url, files) {
-        return console.error("이 코드는 사용되지 않습니다. 대신 siteinfo를 사용하실 수 있습니다.\nThis code is deprecated. You can use 'siteinfo' instead.")
+        new Error("이 코드는 사용되지 않습니다. 대신 siteinfo를 사용하실 수 있습니다.\nThis code is deprecated. You can use 'siteinfo' instead.");
       }
 }
